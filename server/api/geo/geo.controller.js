8e3049697f7ff7c201b0ea149d0b91bea33a874c
MichaelZ <mziyam@mtc.ca.gov>
2016-01-23 03:22:21 -0800

    [Geo] Updates to city and county

--- server/api/geo/geo.controller.js
+++ server/api/geo/geo.controller.js
@@ -7,7 +7,7 @@ var config = require('./../../config/environment');
 // Get list of t6s
 exports.county = function(req, res) {
     var request = new sql.Request(config.mssql.connection);
-    var query = 'SELECT LEFT(Residence_Geo, LEN(Residence_Geo) - 19) AS County FROM VitalSigns.T3_CommuteTime_County GROUP BY LEFT(Residence_Geo, LEN(Residence_Geo) - 19) HAVING (LEFT(Residence_Geo, LEN(Residence_Geo) -19) IS NOT NULL)';
+    var query = 'SELECT distinct [County] FROM [VitalSigns_API].[VS2].[T4_CommuteTimeEmp_County]';
     request.query(query, function(err, data) {
         if (err) {
             console.log(err);
@@ -25,7 +25,7 @@ exports.county = function(req, res) {
 
 exports.city = function(req, res) {
     var request = new sql.Request(config.mssql.connection);
-    var query = 'SELECT LEFT ([Residence_Geo],LEN([Residence_Geo]) - 17) as City FROM [VitalSigns].[T3_CommuteTime_City] ORDER BY [Residence_Geo]';
+    var query = 'SELECT City FROM VitalSigns_API.VS2.T1_CommuteModeShareRes_City WHERE (City IS NOT NULL)';
     request.query(query, function(err, data) {
         if (err) {
             console.log(err);
